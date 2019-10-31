e031a2d70425201839d99a73cbde49dcf0cd55d3
Pommy <15435750+pomeiei@users.noreply.github.com>
2019-10-31 21:09:00 +0700

    TEST
    TEST

--- test2.py
+++ test2.py
@@ -1,8 +1,1 @@
-import pygal
-b_chart = pygal.Bar()
-b_chart.title = "Destiny Kill/Death Ratio"
-b_chart.add("Dijiphos", [0.94])
-b_chart.add("Punisherdonk", [1.05])
-b_chart.add("Musclemuffins20", [1.10])
-b_chart.render_in_browser()
-
+print("TEST UPLOAD")
