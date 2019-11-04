b7dcb93c9073614c6bd607e83934e62ff1f3989f
Warlord5588 <56461130+Warlord5588@users.noreply.github.com>
2019-11-04 20:09:38 +0700

    Update BMi-test.py

--- BMi-test.py
+++ BMi-test.py
@@ -8,4 +8,19 @@ def find_bmi():
     bmi = (weight / (height * height))
     print('Your BMI is %.2f' % bmi)
 
+def tdee():
+	print("")
+	print("This is TDEE Calculater.")
+	print("")
+	gender = input("What is your gender Male or Female ")
+	age = int(input("How old are you "))
+	weight = float(input("Your weight is "))
+	hight = float(input("Your hight is "))
+	bmr = 0
+	"""BMR CALCULATED"""
+	if gender == "Male":
+		bmr = 66 + (13.7 * weight) + (5 * hight) - (6.8 * age)
+	elif gender == "Female":
+		bmr = 665 + (9.6 * weight) + (1.8 * hight) - (4.7 * age)
+
 find_bmi():
