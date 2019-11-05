e153607fca4a5b090bf9d560db5d1a89d57a8cd7
Warlord5588 <56461130+Warlord5588@users.noreply.github.com>
2019-11-05 20:19:25 +0700

    Update BMi-test.py

--- BMi-test.py
+++ BMi-test.py
@@ -23,4 +23,26 @@ def tdee():
 	elif gender == "Female":
 		bmr = 665 + (9.6 * weight) + (1.8 * hight) - (4.7 * age)
 
+	print("Press 1 if you don't have any workout")
+	print("Press 2 if you have a workout 1-3 times per week")
+	print("Press 3 if you have a workout 3-5 times per week")
+	print("Press 4 if you have a workout 6-7 times per week")
+	print("Press 5 if you workout so hard everday")
+	tdee = 0
+	answer = input()
+	if answer == "1":
+		tdee = 1.2 * bmr
+	elif answer == "2":
+		tdee = 1.375 * bmr
+	elif answer == "3":
+		tdee = 1.55 * bmr
+	elif answer == "4":
+		tdee = 1.725 * bmr
+	elif answer == "5":
+		tdee = 1.9 * bmr
+	else:
+		print("Please answer the right choice 1,2,3,4 or 5")
+		tdee()
+	print("Your TDEE is %.2f" %tdee)
+	
 find_bmi():
